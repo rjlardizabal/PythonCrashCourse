bc9afb6febcaefdcae106143525a0e4134915564
rjlardizabal <rj.lardizabal22@gmail.com>
2018-04-26 22:04:27 +0800

    added alien invasion

--- even_or_odd.py
+++ even_or_odd.py
@@ -1,7 +1,7 @@
 number = input('Enter a number, and Ill tell you if its even or odd')
 number = int(number)
 
-if number%2 == 0:
+if number % 2 == 0:
     print("Your number is even")
 else:
     print("Your number is odd")
