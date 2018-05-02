06235beb2118dbf1a26f328b7bf110ecbd181322
rjlardizabal <rj.lardizabal22@gmail.com>
2018-05-02 23:53:29 +0800

    Added bullets functionalites

--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -7,6 +7,7 @@ class Settings():
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
+        self.bullets_allowed = 3
         self.ship_speed_factor = 1.5
 
         # Bullet Settings
