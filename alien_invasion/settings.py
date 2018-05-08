724c14339ff7e554647533147644e703693171f4
rjlardizabal <rj.lardizabal22@gmail.com>
2018-05-08 19:52:59 +0800

    making fleet move

--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -10,6 +10,11 @@ class Settings():
         self.bullets_allowed = 3
         self.ship_speed_factor = 1.5
 
+        # Alien settings
+        self.alien_speed_factor = 1
+        self.fleet_drop_speed = 10
+        self.fleet_direction = 1
+
         # Bullet Settings
         self.bullet_speed_factor = 1
         self.bullet_width = 1
